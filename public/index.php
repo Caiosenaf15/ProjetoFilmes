87723dd4cba2ec1b31a1e5789e799e80083f1b3f
CaioSenaf15 <caiofreitas15@gmail.com>
2026-02-19 10:55:45 -0300

    safeload

--- public/index.php
+++ public/index.php
@@ -11,7 +11,7 @@ $url = trim(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH), '/');
 require_once __DIR__ . '/../vendor/autoload.php';
 
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
-$dotenv->load();
+$dotenv->safeLoad();
 
 
 require_once __DIR__ . '/../routes/web.php';
